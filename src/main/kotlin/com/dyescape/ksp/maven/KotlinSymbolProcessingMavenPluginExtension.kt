80ec259b040ce813c88582d0d2ff58565844fe65
fxshlein <fxshlein@fxshlein.net>
2022-05-08 15:31:41 +0200

    Only set default options when they are not specified by the user

--- src/main/kotlin/com/dyescape/ksp/maven/KotlinSymbolProcessingMavenPluginExtension.kt
+++ src/main/kotlin/com/dyescape/ksp/maven/KotlinSymbolProcessingMavenPluginExtension.kt
@@ -1,6 +1,7 @@
 package com.dyescape.ksp.maven
 
 import com.google.devtools.ksp.KspCliOption
+import com.google.devtools.ksp.KspCliOption.*
 import org.apache.maven.artifact.Artifact
 import org.apache.maven.artifact.resolver.ArtifactResolutionRequest
 import org.apache.maven.plugin.MojoExecution
@@ -12,13 +13,16 @@ import org.jetbrains.kotlin.maven.KotlinMavenPluginExtension
 import org.jetbrains.kotlin.maven.PluginOption
 import java.io.File
 
+const val KSP_PLUGIN_NAME = "ksp"
 const val KSP_PLUGIN_ID = "com.google.devtools.ksp.symbol-processing"
 
 /**
  * Extension that enables KSP for the kotlin maven plugin
  */
-@Component(role = KotlinMavenPluginExtension::class, hint = "ksp")
+@Component(role = KotlinMavenPluginExtension::class, hint = KSP_PLUGIN_NAME)
 class KotlinSymbolProcessingMavenPluginExtension : KotlinMavenPluginExtension {
+    private val outputDirOptions = arrayOf(JAVA_OUTPUT_DIR_OPTION, KOTLIN_OUTPUT_DIR_OPTION)
+
     @Requirement
     lateinit var system: RepositorySystem
 
@@ -27,20 +31,36 @@ class KotlinSymbolProcessingMavenPluginExtension : KotlinMavenPluginExtension {
     override fun getCompilerPluginId() = KSP_PLUGIN_ID
 
     override fun getPluginOptions(project: MavenProject, execution: MojoExecution): List<PluginOption> {
-        val options = mutableMapOf<KspCliOption, String>()
+        val userOptions = execution.findKspOptions()
 
-        options[KspCliOption.CLASS_OUTPUT_DIR_OPTION] = project.build.outputDirectory
-        options[KspCliOption.JAVA_OUTPUT_DIR_OPTION] = project.generatedSourcesDir("ksp-java").path
-        options[KspCliOption.KOTLIN_OUTPUT_DIR_OPTION] = project.generatedSourcesDir("ksp").path
-        options[KspCliOption.RESOURCE_OUTPUT_DIR_OPTION] = project.build.outputDirectory
-        options[KspCliOption.PROJECT_BASE_DIR_OPTION] = project.basedir.path
-        options[KspCliOption.KSP_OUTPUT_DIR_OPTION] = project.buildDir("ksp").path
-        options[KspCliOption.PROCESSOR_CLASSPATH_OPTION] = execution.constructProcessorClassPath()
-        options[KspCliOption.CACHES_DIR_OPTION] = system.cacheDir("ksp").path
-        options[KspCliOption.WITH_COMPILATION_OPTION] = "true"
+        val defaultOptions = mutableMapOf<KspCliOption, String>()
 
-        project.addCompileSourceRoot(options[KspCliOption.JAVA_OUTPUT_DIR_OPTION])
-        project.addCompileSourceRoot(options[KspCliOption.KOTLIN_OUTPUT_DIR_OPTION])
+        defaultOptions[CLASS_OUTPUT_DIR_OPTION] = project.build.outputDirectory
+        defaultOptions[JAVA_OUTPUT_DIR_OPTION] = project.generatedSourcesDir("ksp-java").path
+        defaultOptions[KOTLIN_OUTPUT_DIR_OPTION] = project.generatedSourcesDir("ksp").path
+        defaultOptions[RESOURCE_OUTPUT_DIR_OPTION] = project.build.outputDirectory
+        defaultOptions[PROJECT_BASE_DIR_OPTION] = project.basedir.path
+        defaultOptions[KSP_OUTPUT_DIR_OPTION] = project.buildDir("ksp").path
+        defaultOptions[PROCESSOR_CLASSPATH_OPTION] = execution.constructProcessorClassPath()
+        defaultOptions[CACHES_DIR_OPTION] = system.cacheDir("ksp").path
+        defaultOptions[WITH_COMPILATION_OPTION] = "true"
+
+        val options = buildMap {
+            for ((option, defaultValue) in defaultOptions) {
+                if (option in userOptions) {
+                    // the option was already specified by the user
+                    continue
+                }
+
+                put(option, defaultValue)
+            }
+        }
+
+        for (outputDirOption in outputDirOptions) {
+            val dir = options[outputDirOption] ?: userOptions[outputDirOption]?.singleOrNull()
+
+            project.addCompileSourceRoot(dir)
+        }
 
         return options.map { (option, value) -> PluginOption("ksp", KSP_PLUGIN_ID, option.optionName, value) }
     }
